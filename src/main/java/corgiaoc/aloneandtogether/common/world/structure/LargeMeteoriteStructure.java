a8d320980231e4a3d576bebc3d97a6a44913d78a
Corgi Taco <66983020+CorgiTaco@users.noreply.github.com>
2021-01-03 22:21:58 -0800

    -Annoying box meteorite

--- src/main/java/corgiaoc/aloneandtogether/common/world/structure/LargeMeteoriteStructure.java
+++ src/main/java/corgiaoc/aloneandtogether/common/world/structure/LargeMeteoriteStructure.java
@@ -49,9 +49,9 @@ public class LargeMeteoriteStructure extends Structure<NoisySphereConfig> {
             BlockPos startPos = new BlockPos(xCoord, 64, zCoord);
             BlockPos.Mutable mutable = new BlockPos.Mutable();
 
-            int xRadius = config.getRandomXRadius(random);
-            int yRadius = config.getRandomYRadius(random);
-            int zRadius = config.getRandomZRadius(random);
+            int xRadius = /*config.getRandomXRadius(random)*/ 50;
+            int yRadius = /*config.getRandomYRadius(random)*/ 50;
+            int zRadius = /*config.getRandomZRadius(random)*/ 50;
 
             int xCurrentRadius = -xRadius;
             int yCurrentRadius = -yRadius;
@@ -71,23 +71,17 @@ public class LargeMeteoriteStructure extends Structure<NoisySphereConfig> {
             this.bounds.maxY = startPos.getY() + yRadius;
             this.bounds.maxZ = startPos.getZ() + zRadius;
 
-            int moveX = 0;
             for (int x = -xComponentMove; x < xComponentMove; x++) {
-                meteoriteDataHelper.setXCurrentRadius(-xRadius + moveX);
-                int moveZ = 0;
+                meteoriteDataHelper.setXCurrentRadius(x * 16);
                 for (int z = -zComponentMove; z < zComponentMove; z++) {
-                    meteoriteDataHelper.setZCurrentRadius(-zRadius + moveZ);
-                    int moveY = 0;
+                    meteoriteDataHelper.setZCurrentRadius(z * 16);
                     for (int y = -yComponentMove; y < yComponentMove; y++) {
-                        mutable.setPos(startPos.getX() + moveX, startPos.getY() + moveY, startPos.getZ() + moveZ);
-                        meteoriteDataHelper.setYCurrentRadius(-yRadius + moveY);
+                        meteoriteDataHelper.setYCurrentRadius(y * 16);
+                        mutable.setPos(startPos.getX() + (x * 16), startPos.getY() + (y * 16), startPos.getZ() + (z * 16));
                         this.components.add(new MeteoritePiece(meteoriteDataHelper, config, mutable.toImmutable()));
-                        moveY += 16;
 
                     }
-                    moveZ += 16;
                 }
-                moveX += 16;
             }
             this.recalculateStructureSize();
         }
@@ -142,7 +136,6 @@ public class LargeMeteoriteStructure extends Structure<NoisySphereConfig> {
             int xCurrentRadius = currentRadiusData.getXCurrentRadius() - xRadius;
             int yCurrentRadius = currentRadiusData.getYCurrentRadius() - yRadius;
             int zCurrentRadius = currentRadiusData.getZCurrentRadius() - zRadius;
-
             boolean isHighestIteration = yCurrentRadius + 16 > yRadius;
 
 
@@ -150,11 +143,19 @@ public class LargeMeteoriteStructure extends Structure<NoisySphereConfig> {
                 int[][] topY = new int[17][17];
                 for (int z = 0; z < 16; z++) {
                     for (int y = 0; y < 16; y++) {
+                        int currentXRadius = xCurrentRadius + x;
+                        int currentYRadius = yCurrentRadius + y;
+                        int currentZRadius = zCurrentRadius + z;
+
+                        if (currentXRadius >= xRadius || currentYRadius >= yRadius || currentZRadius >= zRadius)
+                            continue;
+
                         mutable2.setPos(mutable).move(x, y, z);
 
                         //Credits to Hex_26 for this equation!
-                        double equationResult = Math.pow(xCurrentRadius + x, 2) / Math.pow(xRadius, 2) + Math.pow(yCurrentRadius + y, 2) / Math.pow(yRadius, 2) + Math.pow(zCurrentRadius + z, 2) / Math.pow(zRadius, 2);
-                         if (equationResult >= 1 + 0.7 * Start.fastNoise.GetNoise(mutable2.getX(), mutable2.getY(), mutable2.getZ()))
+                        double equationResult = Math.pow(currentXRadius, 2) / Math.pow(xRadius, 2) + Math.pow(currentYRadius, 2) / Math.pow(yRadius, 2) + Math.pow(currentZRadius, 2) / Math.pow(zRadius, 2);
+                        double threshold = 1 + 0.7 * Start.fastNoise.GetNoise(mutable2.getX(), mutable2.getY(), mutable2.getZ());
+                        if (equationResult >= threshold)
                             continue;
 
                         world.setBlockState(mutable2, config.getBlockProvider().getBlockState(random, mutable2), 2);
