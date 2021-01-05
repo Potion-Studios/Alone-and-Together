ef9c97f5a0e59194b929de59b1306262fae83d15
Corgi Taco <66983020+CorgiTaco@users.noreply.github.com>
2021-01-04 17:51:51 -0800

    -Fix large meteorite structures

--- src/main/java/corgiaoc/aloneandtogether/core/world/ATConfiguredStructures.java
+++ src/main/java/corgiaoc/aloneandtogether/core/world/ATConfiguredStructures.java
@@ -8,6 +8,6 @@ import net.minecraft.world.gen.feature.StructureFeature;
 
 public class ATConfiguredStructures {
 
-    public static final StructureFeature<?, ?> LARGE_METEORITE = WorldGenRegistrationHelper.createConfiguredStructureFeature("large_meteorite", ATStructures.LARGE_METEORITE.withConfiguration(new NoisySphereConfig.Builder().setMinXRadius(105).setMaxXRadius(135).setMinYRadius(55).setMaxYRadius(65).setMinZRadius(100).setMaxZRadius(125).setBlock(new WeightedBlockStateProvider().addWeightedBlockstate(Blocks.BLACKSTONE.getDefaultState(), 5).addWeightedBlockstate(Blocks.BLACK_CONCRETE.getDefaultState(), 5).addWeightedBlockstate(Blocks.QUARTZ_BLOCK.getDefaultState(), 2).addWeightedBlockstate(Blocks.QUARTZ_BLOCK.getDefaultState(), 10)).setTopBlock(Blocks.QUARTZ_BLOCK).build()));
+    public static final StructureFeature<?, ?> LARGE_METEORITE = WorldGenRegistrationHelper.createConfiguredStructureFeature("large_meteorite", ATStructures.LARGE_METEORITE.withConfiguration(new NoisySphereConfig.Builder().setMinXRadius(40).setMaxXRadius(54).setMinYRadius(35).setMaxYRadius(55).setMinZRadius(40).setMaxZRadius(54).setBlock(new WeightedBlockStateProvider().addWeightedBlockstate(Blocks.BLACKSTONE.getDefaultState(), 5).addWeightedBlockstate(Blocks.BLACK_CONCRETE.getDefaultState(), 5).addWeightedBlockstate(Blocks.DEAD_BUBBLE_CORAL_BLOCK.getDefaultState(), 2).addWeightedBlockstate(Blocks.DEAD_BRAIN_CORAL_BLOCK.getDefaultState(), 10)).setTopBlock(Blocks.QUARTZ_BLOCK).setNoiseFrequency(0.03).build()));
 
 }
